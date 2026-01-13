ae4c7e02ec70b223373c4673e87159bc875522d0
danielnoblenava <danielnoble290@gmail.com>
2026-01-12 23:42:18 -0600

    Implement read_full and write_all for TCP server
    Refactored TCP server to handle full messages and replies.

--- server-client/TCP_server.c
+++ server-client/TCP_server.c
@@ -8,6 +8,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <assert.h>
+
 
 static void msg(const char *msg){
     fprintf(stderr, "%s\n", msg);
@@ -19,16 +21,70 @@ static void die(const char *msg){
     abort();
 }
 
-static void do_something(int connfd){
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) -1);
-    if(n < 0){
-        msg("read() error");
-        return;
-    }
-    fprintf(stderr, "client says: %s\n", rbuf);
-    char wbuf[] = "world";
-    write(connfd, wbuf, strlen(wbuf));
+const size_t k_max_msg = 4096;
+
+static int32_t read_full(int fd, char *buf, size_t n){
+	while (n > 0){
+		ssize_t rv = read(fd, buf, n);
+		if (rv <= 0){
+			return -1; // error, or unexpected EOF
+		}
+		assert((size_t)rv <= n);
+		n -= (size_t)rv;
+		buf += rv;
+	}
+	return 0;
+	
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n){
+	while (n > 0){
+		ssize_t rv = write(fd, buf, n);
+		if (rv <= 0){
+			return -1;
+		}
+		assert((size_t)rv <= n);
+		n -= (size_t)rv;
+		buf += rv;
+	}
+	return 0;
+}
+
+static int32_t one_request(int connfd){
+	//4 bytes header
+	char rbuf[4 + k_max_msg];
+	errno = 0;
+	int32_t err = read_full(connfd, rbuf, 4);
+	if (err){
+		msg(errno == 0 ? "EOF" : "read() error");
+		return err;
+	}
+	
+	uint32_t len = 0;
+	memcpy(&len, rbuf, 4); //assume little endian
+	if (len > k_max_msg){
+		msg("too long");
+		return -1;
+	}
+
+	
+	// request body
+	err = read_full(connfd, &rbuf[4], len);
+	if (err){
+		msg("read() error");
+		return err;
+	}
+
+	//do something
+	fprintf(stderr, "client says: %. *s\n", len, &rbuf[4]);
+	
+	// reply using the same protocol
+	const char reply[] = "jijijaja";
+	char wbuf[4 + sizeof(reply)];
+	len = (uint32_t)strlen(reply);
+	memcpy(wbuf, &len, 4);
+	memcpy(&wbuf[4], &reply, len);
+	return write_all(connfd, wbuf, 4 + len);
 }
 
 int main(){
@@ -80,10 +136,18 @@ int main(){
         if(connfd < 0){
             continue; //error
         }
-        do_something(connfd);
-        close(connfd);
+
+	while(true){
+		//here the server only serves one client connection at once
+		int32_t err = one_request(connfd);
+		if (err){
+			break;
+		}
+	}
+	close(connfd);
     }
     
     return 0;
 }
+
 
